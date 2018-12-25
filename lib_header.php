baddbf075a681de56ca1864a64108b812f8cdd2f
herlambang6661 <herlambang6661@gmail.com>
2018-12-25 18:09:11 +0700

    sweetAlert header

--- lib_header.php
+++ lib_header.php
@@ -13,3 +13,8 @@
     <link rel="stylesheet" href="assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="assets/vendor/charts/c3charts/c3.css">
     <link rel="stylesheet" href="assets/vendor/fonts/flag-icon-css/flag-icon.min.css">
+
+
+    <!-- SWEET ALERT -->
+    <link rel="stylesheet" href="bower_components/sweetalert2/dist/sweetalert2.min.css">
+    <script src="bower_components/sweetalert2/dist/sweetalert2.min.js"></script>
